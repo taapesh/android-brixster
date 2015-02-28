cbc11d700e20ea541cec1ee2288fd995cc92616a
Arya Mohades <taapesh@Aryas-MacBook-Pro.local>
2015-02-28 12:20:43 -0600

    Added a GPStracker class for obtaining latitude and longitude. Added permissions to manifest.

--- app/src/main/java/com/example/taapesh/prototype/HomepageCustomer.java
+++ app/src/main/java/com/example/taapesh/prototype/HomepageCustomer.java
@@ -4,16 +4,51 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 // Imports for Google Places
 
 
 public class HomepageCustomer extends ActionBarActivity {
 
+    protected Button showGPSButton;
+
+    // GPS tracker class
+    GPSTracker gps;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_customer);
+
+        showGPSButton = (Button) findViewById(R.id.showGPSButton);
+
+        showGPSButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create gps class object
+                gps = new GPSTracker(HomepageCustomer.this);
+
+                // Check if GPS enabled
+                if(gps.canGetLocation()) {
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // Show location using toast
+                    Toast.makeText(getApplicationContext(),
+                            "Your Location \nLat: " + latitude +
+                            "\nLong: " + longitude,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    // Can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+            }
+        });
     }
 
     @Override
